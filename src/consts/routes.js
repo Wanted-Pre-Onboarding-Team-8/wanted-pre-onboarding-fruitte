d63ebd6c4451eb19eddbad869dd356e15112a369
J'aimemin <61978339+Kim-Jaemin420@users.noreply.github.com>
2022-09-04 12:51:59 +0900

    Feature/api mocking (#2)
    * chore: install msw
    
    * chore: msw init
    
    * feat: routes 상수 추가
    
    * chore: git ingore .env file
    
    * feat: msw api handlers 구현
    
    * feat: msw 기본 파일 설정
    
    * chore: dummy data 추가
    
    * chore: empty 파일 삭제

---
+++ src/consts/routes.js
@@ -0,0 +1,4 @@
+export const PRODUCTS_PAGE = '/products';
+export const PAYMENT_PAGE = '/products/payment';
+export const ADMIN_PAGE = '/admin';
+export const PRODUCT_ADD_PAGE = '/admin/product_add';
