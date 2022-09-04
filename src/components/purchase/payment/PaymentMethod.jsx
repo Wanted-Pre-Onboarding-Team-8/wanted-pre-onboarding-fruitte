67363da027f149e4d3e4f929dc03ec78a4d37a1e
HyeonSeong <xkdtndua15@gmail.com>
2022-09-05 04:03:51 +0900

    fix: 약관동의 체크박스 모두동의 체크시 라디오버튼 초기화 현상 수정

--- src/components/purchase/payment/PaymentMethod.jsx
+++ src/components/purchase/payment/PaymentMethod.jsx
@@ -1,13 +1,30 @@
 import { PaymentMethodForm, PaymentItemForm } from './paymentStyle';
+import { useState } from 'react';
 
 function PaymentMethod() {
+  const [currentValue, setCurrentValue] = useState('card');
+
   return (
     <PaymentItemForm>
       <h2>결제수단</h2>
       <PaymentMethodForm>
-        <input type="radio" name="payment-method" checked onChange={() => {}} />
+        <input
+          type="radio"
+          name="payment-method"
+          checked={currentValue === 'card'}
+          onChange={() => {
+            setCurrentValue('card');
+          }}
+        />
         <label htmlFor="card">신용카드</label>
-        <input type="radio" name="payment-method" />
+        <input
+          type="radio"
+          name="payment-method"
+          checked={currentValue === 'cash'}
+          onChange={() => {
+            setCurrentValue('cash');
+          }}
+        />
         <label htmlFor="cash">무통장입금</label>
       </PaymentMethodForm>
     </PaymentItemForm>
