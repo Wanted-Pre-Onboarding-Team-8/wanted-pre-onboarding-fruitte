61f53ffd2919d82c272c601a0cb24a1d95985a76
HyeonSeong <xkdtndua15@gmail.com>
2022-09-05 02:12:39 +0900

    chore: 결제 내역 url 생성

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import {
   ADMIN_PAGE,
   PAYMENT_PAGE,
+  PAYMENT_RESULT_PAGE,
   PRODUCTS_PAGE,
   PRODUCT_ADD_PAGE,
 } from './consts';
@@ -15,6 +16,7 @@ function App() {
         <Route path={PRODUCTS_PAGE} element={<div>메인</div>} />
         <Route path={`${PRODUCTS_PAGE}/:id`} element={<div>상세</div>} />
         <Route path={PAYMENT_PAGE} element={<Purchase />} />
+        <Route path={PAYMENT_RESULT_PAGE} element={<div>주문목록</div>} />
         <Route path={ADMIN_PAGE} element={<Admin />} />
         <Route path={PRODUCT_ADD_PAGE} element={<div>관리자 상품 추가</div>} />
         <Route path="/*" element={<div>404 Not Fround</div>} />
