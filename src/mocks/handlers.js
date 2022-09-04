1f3717e82a58d11957a0bcad7e6f244973cc51a5
dlwldn <00ljw00@naver.com>
2022-09-04 15:28:09 +0900

    feat: api mock 페이지네이션 추가
    메인 페이지에 필요한 상품 api 페이지네이션 처리

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,11 +1,28 @@
 import { rest } from 'msw';
-import { PRODUCTS_PAGE, PRODUCT_ADD_PAGE } from '../consts';
+import { PRODUCTS_PAGE, PRODUCT_ADD_PAGE, PRODUCT_API_URL } from '../consts';
 import data from './data.json';
 
 const productList = [...data.productList];
 const productDetail = [...data.productDetail];
 
 export const handlers = [
+  rest.get(PRODUCT_API_URL, (req, res, ctx) => {
+    const page = Number(req.url.searchParams.get('page'));
+    const size = Number(req.url.searchParams.get('size'));
+    const totalPage = Math.ceil(productList.length / size);
+    const newProducts = [...productList];
+    const targetProductsList = newProducts.splice(page * size, size);
+
+    const responseData = {
+      totalPage,
+      size,
+      currPage: page,
+      products: targetProductsList,
+    };
+
+    return res(ctx.status(200), ctx.json(responseData));
+  }),
+
   rest.get(`${PRODUCTS_PAGE}?id=`, async (req, res, ctx) => {
     const currentProductId = req.url.searchParams.get('id');
     const productDetailById = productDetail.find(
