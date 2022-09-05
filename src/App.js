bdbc9a2c469eb1200f752cf8b28f160bf85c31a9
metacode22 <93233930+metacode22@users.noreply.github.com>
2022-09-05 11:20:49 +0900

    feat: Navigation Bar 구현 (#19)

--- src/App.js
+++ src/App.js
@@ -6,24 +6,48 @@ import {
   PRODUCTS_PAGE,
   PRODUCT_ADD_PAGE,
 } from './consts';
+import styled from 'styled-components';
 import Purchase from './pages/Purchase';
 import Admin from './pages/Admin';
 import Products from './pages/Products';
+import Nav from './components/common/Nav/Nav';
 
 function App() {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path={PRODUCTS_PAGE} element={<Products />} />
         <Route path={`${PRODUCTS_PAGE}/:id`} element={<div>상세</div>} />
         <Route path={PAYMENT_PAGE} element={<Purchase />} />
-        <Route path={PAYMENT_RESULT_PAGE} element={<div>주문목록</div>} />
+        <Route
+          path={PAYMENT_RESULT_PAGE}
+          element={
+            <Wrapper>
+              <div>주문목록</div>
+            </Wrapper>
+          }
+        />
         <Route path={ADMIN_PAGE} element={<Admin />} />
         <Route path={PRODUCT_ADD_PAGE} element={<div>관리자 상품 추가</div>} />
-        <Route path="/*" element={<div>404 Not Fround</div>} />
+
+        <Route
+          path="/*"
+          element={
+            <Wrapper>
+              <div>상단 메뉴를 클릭해서 이동해주세요.</div>
+            </Wrapper>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
 }
+
+export const Wrapper = styled.div`
+  width: 100%;
+  text-align: center;
+  margin-top: 100px;
+`;
 
 export default App;
